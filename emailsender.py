e44395bb8e0352f78ad0b778763d8897c7e272a2
ali21-gmu <82505566+ali21-gmu@users.noreply.github.com>
2021-04-15 09:59:03 -0400

    Update emailsender.py

--- emailsender.py
+++ emailsender.py
@@ -1,16 +1,23 @@
 import smtplib, ssl
+from email.message import EmailMessage
 
-port = 465  # For SSL
-smtp_server = "smtp.gmail.com"
-sender_email = "cs321testgroup3@gmail.com"  # Enter your address
-receiver_email = "alee79808@gmail.com"  # Enter receiver address
-password = input("Type your password and press enter: ")
-message = """\
-Subject: Hi there
-
-This message is sent from Python."""
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+def emailsender():
+	port = 465  # For SSL
+	smtp_server = "smtp.gmail.com"
+	sender = "cs321testgroup3@gmail.com"
+	print("Default email is cs321testgroup3@gmail.com")
+	receiver = input("Enter the email address of the receiving party: ")
+	message = EmailMessage()
+	message['Subject'] = input("Subject of your email: ")
+	message['From'] = sender
+	message['To'] = receiver
+	message.set_content(input("Main Message: "))
+	password = input("Type your password and press enter: ")
+	
+	context = ssl.create_default_context()
+	try:
+		with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+			server.login(sender, password)
+			server.send_message(message)
+	except Exception as e:
+		print(e)
